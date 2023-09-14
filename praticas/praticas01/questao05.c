5572ee0af7962f38fa44e76a5e6e9c7ff1c95e90
BJbrun <142457862+BJbrun@users.noreply.github.com>
2023-09-14 00:40:01 +0000

    ..

--- praticas/praticas01/questao05.c
+++ praticas/praticas01/questao05.c
@@ -1,14 +1,14 @@
 // Faça um progtama em C que converta um valor qualquer em Gigabytes para um valor em bytes : (1GB = 1024bytes).
 
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-  float GB = 0.1024;
-  float valor_gb = 4;
+  int valor_em_giga = 10;
   
-  float gigabytes = 4 * GB
-
-  printf(" O valor é %f\n"), gigabytes;
+  double valor_em_bytes = valor_em_giga = pow(1024, 3) ;
+  
+  printf(" O valor  %d em gigabytes equivale a %f bytes\n", valor_em_giga, valor_em_bytes);
 
    return 0;
  }
---
+++ praticas/praticas01/questao06.c
@@ -0,0 +1,19 @@
+// faça um programa em C que calcule as raizes de uma equação de 2 grau atraves da fórmula de bhaskara(-b +/- raiz_quadrada(b ao quadrado -4ac)/2a)
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  int coeficiente_A = -1;
+  int coeficiente_B = 2;
+  int coeficiente_C = 1;
+
+  double delta = pow(coeficiente_B, 2) - 4 * coeficiente_A * coeficiente_C;
+  double raiz1 = (- coeficiente_B + sqrt(delta))/2 * coeficiente_A;
+  double raiz2 = (- coeficiente_B - sqrt(delta))/2 * coeficiente_A;
+
+  printf("As raizes da equação %dx *2 %dx %d são %f e %f\n", coeficiente_A, coeficiente_B, coeficiente_C, raiz1, raiz2);
+
+
+   return 0;
+ }
